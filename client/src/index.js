4d27871d8891d394c0ac4abdb4337e487f2cb2c7
aceteam.dfs@gmail.com <eugene@Eugenes-MacBook-Pro-2.local>
2020-12-26 15:00:17 -0600

    moved redirect_url to .env file

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import { BrowserRouter} from "react-router-dom";
 import 'antd/dist/antd.css';
+// import firebase from "firebase/app";
+// import "firebase/auth";
+
+// const config = {
+//       apiKey: "AIzaSyCi47Tf4at14D8XUBvUgkEBqz9asusr1vE",
+//       authDomain: "ecommerce-5a933.firebaseapp.com",
+//       projectId: "ecommerce-5a933",
+//       storageBucket: "ecommerce-5a933.appspot.com",
+//       messagingSenderId: "418241998625",
+//       appId: "1:418241998625:web:f382c99d1be11eb0779860"
+//     };
+//     // Initialize Firebase
+//     if (!firebase.apps.length) {
+//       firebase.initializeApp(config);
+      
+//           }
 
 ReactDOM.render(
   //<React.StrictMode>
